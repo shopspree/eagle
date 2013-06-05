931542d97d3ee62ee400aaa5e8471b8764acea6e
asaf.am <asaf.ach@gmail.com>
2013-06-05 17:23:46 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eagle::Application.config.secret_token = 'd0574bbb09839c2daac263938dc4882d4dffacb0522f888970e5a1a7961ab047ace1a6dc05cf5a0864aa7e1c13543b46cb3df6bbb4f9343d6fddf3e8892062f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Eagle::Application.config.session_store :cookie_store, key: '_eagle_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Eagle::Application.config.session_store :active_record_store
