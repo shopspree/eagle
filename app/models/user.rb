5deb72801c12cf6af369050714ab8cae3d1abcce
asaf.am <asaf.ach@gmail.com>
2013-06-11 13:19:30 +0300

    Devise fix setup script to create the table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :token_authenticatable,
+         :confirmable,
          :registerable,
          :recoverable,
          :rememberable,
