5727e6d74d5c31a5a2fcc5613b0dc374cae362e7
asaf.am <asaf.ach@gmail.com>
2013-06-15 11:31:50 +0300

    deleted files

--- app/models/activity_activity_object.rb
+++
@@ -1,7 +0,0 @@
-class ActivityActivityObject < ActiveRecord::Base
-  belongs_to :activity
-  belongs_to :activity_object
-
-  attr_accessible :activity_id, :activity_object_id
-
-end
--- db/migrate/20130612151944_add_user_id_to_actor.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToActor < ActiveRecord::Migration
-  def change
-    add_column :actors, :user_id, :integer
-  end
-end
